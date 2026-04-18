ae3fded316e0c9f918da728935877350733f80c3
DaniaSalamhdr4 <152274979+DaniaSalamhdr4@users.noreply.github.com>
2026-04-18 14:17:06 +0300

    programming PI and E buttons

--- js/main.js
+++ js/main.js
@@ -14,25 +14,11 @@ arr.forEach((button) => {
     } else if (e.target.innerHTML == "DEL") {
       string = string.substring(0, string.length - 1);
       input.value = string;
-    } else if (e.target.innerHTML == "CE") {
-      let lastOperatorIndex = Math.max(
-        string.lastIndexOf("+"),
-        string.lastIndexOf("-"),
-        string.lastIndexOf("*"),
-        string.lastIndexOf("/"),
-      );
-
-      if (lastOperatorIndex !== -1) {
-        string = string.substring(0, lastOperatorIndex + 1);
-      } else {
-        string = "";
-      }
-      input.value = string;
     } else if (e.target.innerHTML == "√") {
       string = Math.sqrt(parseFloat(string)).toString();
       input.value = string;
     } else if (e.target.innerHTML == "x²") {
-      string = parseFloat(string) * parseFloat(string).toString();
+      string = (parseFloat(string) ** 2).toString();
       input.value = string;
     } else if (e.target.innerHTML == "1/x") {
       if (string === "" || string === "0") {
@@ -42,10 +28,15 @@ arr.forEach((button) => {
         string = (1 / parseFloat(string)).toString();
         input.value = string;
       }
+    } else if (e.target.innerHTML == "π") {
+      string += Math.PI.toString();
+      input.value = string;
+    } else if (e.target.innerHTML == "e") {
+      string += Math.E.toString();
+      input.value = string;
     } else {
       string += e.target.innerHTML;
       input.value = string;
     }
-    
   });
 });
